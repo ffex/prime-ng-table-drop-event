3426a95c0ad3ee1b3c798746a03c850e54049b94
Federico Bassini <federico.bassini@01s.eu>
2023-01-28 14:29:50 +0100

    Removed console logs. Code formatted.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product.model';
 import { ProductService } from './product.service';
-import {ReordableRowDrop} from './reordable-row-drop.directive'
+import { ReordableRowDrop } from './reordable-row-drop.directive'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'prime-ng-table-drop-event';
 
   products: Product[] = [];
 
-  cols: any[]=[];
-  logs:string[]=[];
+  cols: any[] = [];
+  logs: string[] = [];
 
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
     // ORDER BY deliveryStatusCode the products
-      this.productService.getProductsSmall().then(data => {
-        this.products = data.sort((a,b) => a.deliveryStatusCode! > b.deliveryStatusCode! ? -1 :1);
-      });
+    this.productService.getProductsSmall().then(data => {
+      this.products = data.sort((a, b) => a.deliveryStatusCode! > b.deliveryStatusCode! ? -1 : 1);
+    });
 
-      this.cols = [
-          { field: 'name', header: 'Name' },
-          { field: 'code', header: 'Code' },
-          { field: 'category', header: 'Category' },
-          { field: 'deliveryStatusCode', header: 'Status Code' },
-          { field: 'deliveryStatus', header: 'Status' }
-      ];
+    this.cols = [
+      { field: 'name', header: 'Name' },
+      { field: 'code', header: 'Code' },
+      { field: 'category', header: 'Category' },
+      { field: 'deliveryStatusCode', header: 'Status Code' },
+      { field: 'deliveryStatus', header: 'Status' }
+    ];
 
   }
 
-  onDropRow(e:any):void{
-    if(this.products[e.draggedRowIndex].deliveryStatusCode! <50 && this.products[e.droppedRowIndex].deliveryStatusCode! <50){
+  onDropRow(e: any): void {
+    if (this.products[e.draggedRowIndex].deliveryStatusCode! < 50 && this.products[e.droppedRowIndex].deliveryStatusCode! < 50) {
       e.sender.acceptDrop();
       this.logs.push(`SUCCESS: Reordered ${this.products[e.draggedRowIndex].name!} in position ${e.droppedRowIndex}.`);
-    }else{
+    } else {
       e.sender.rejectDrop();
       this.logs.push(`FAILED: Impossibile move ${this.products[e.draggedRowIndex].name!} in position ${e.droppedRowIndex}. One of those items was delivered yet. :(`);
     }
--- src/app/reordable-row-drop.directive.ts
+++ src/app/reordable-row-drop.directive.ts
@@ -1,6 +1,6 @@
-import {ElementRef, EventEmitter, Input, NgZone, Output} from '@angular/core';
-import {Directive, HostListener} from '@angular/core';
-import {ReorderableRow, Table} from 'primeng/table';
+import { ElementRef, EventEmitter, Input, NgZone, Output } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
+import { ReorderableRow, Table } from 'primeng/table';
 
 
 @Directive({
@@ -14,8 +14,8 @@ export class ReordableRowDrop extends ReorderableRow {
   @Output() dropRow = new EventEmitter<any>();
   override index = 0;
   tmsEventDrop: any;
-  dtDraggedRowIndex?:number;
-  dtDroppedRowIndex?:number;
+  dtDraggedRowIndex?: number;
+  dtDroppedRowIndex?: number;
 
   constructor(public override dt: Table, public override el: ElementRef, public override zone: NgZone) {
     super(dt, el, zone);
@@ -23,51 +23,40 @@ export class ReordableRowDrop extends ReorderableRow {
 
   //@HostListener('drop', ['$event'])
   override onDrop(event: any): void {
-    console.log("#### SONO QUA DIRETTIVA!");
-    console.log(event);
+
     if (this.isEnabled() && this.dt.rowDragging) {
-        console.log(this.dt.droppedRowIndex);
-        this.dtDraggedRowIndex=this.dt.draggedRowIndex;
-        this.dtDroppedRowIndex=this.dt.droppedRowIndex;
-        const dropIndex = this.dt.draggedRowIndex > this.dt.droppedRowIndex ? this.dt.droppedRowIndex : this.dt.droppedRowIndex === 0 ? 0 : this.dt.droppedRowIndex - 1;
+      this.dtDraggedRowIndex = this.dt.draggedRowIndex;
+      this.dtDroppedRowIndex = this.dt.droppedRowIndex;
+      const dropIndex = this.dt.draggedRowIndex > this.dt.droppedRowIndex ? this.dt.droppedRowIndex : this.dt.droppedRowIndex === 0 ? 0 : this.dt.droppedRowIndex - 1;
 
-        this.tmsEventDrop = event;
+      this.tmsEventDrop = event;
 
-        const eventArgs = {
-            sender : this,
-            draggedRowIndex: this.dt.draggedRowIndex,
-            droppedRowIndex: dropIndex
-        }
-        
-        //new tmsPTableDropEventArgs(this, this.dt.draggedRowIndex, dropIndex);
-        this.dropRow.emit(eventArgs);
+      const eventArgs = {
+        sender: this,
+        draggedRowIndex: this.dt.draggedRowIndex,
+        droppedRowIndex: dropIndex
+      }
+
+      this.dropRow.emit(eventArgs);
 
 
     }
 
     event.preventDefault();
 
-    //event.preventDefault();
-
   }
 
   public acceptDrop(): void {
-    console.log("#### Accettata!!!");
-    console.log(this.tmsEventDrop);
-    console.log(this.el);
-    this.dt.draggedRowIndex=this.dtDraggedRowIndex!;
-    this.dt.droppedRowIndex=this.dtDroppedRowIndex!;
-    console.log(this.dt.droppedRowIndex);
+    this.dt.draggedRowIndex = this.dtDraggedRowIndex!;
+    this.dt.droppedRowIndex = this.dtDroppedRowIndex!;
     this.dt.onRowDrop(this.tmsEventDrop, this.el.nativeElement);
-    //this.tmsEventDrop.preventDefault();
-
   }
 
   public rejectDrop(): void {
     //cleanup
-    this.tmsEventDrop=null;
-    this.dtDraggedRowIndex=undefined;
-    this.dtDroppedRowIndex=undefined;
+    this.tmsEventDrop = null;
+    this.dtDraggedRowIndex = undefined;
+    this.dtDroppedRowIndex = undefined;
     this.dt.onRowDragLeave(this.tmsEventDrop, this.el.nativeElement);
     this.dt.onRowDragEnd(this.tmsEventDrop);
   }
